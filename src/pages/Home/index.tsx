306ef4a6de8543e81d13c65c3f6baf54012b77d5
Wolgrand Neto <wolgrand@live.com>
2020-10-16 00:25:46 -0300

    Add: Home Page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import {FiPower} from 'react-icons/fi'
+
+import { Container, Header, Filter, Title, Main, Footer } from './styles';
 
 const Home: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Header >
+        <p>EasySeat</p>
+        <FiPower />
+      </Header>
+      <Title>
+        Eventos
+      </Title>
+      <Filter>
+        <button>Todos</button>
+        <button>Próximos</button>
+        <button>Passados</button>
+      </Filter>
+      <Main>
+        <div></div>
+      </Main>
+      <Footer>
+        Footer
+      </Footer>
+    </Container>
+  );
 }
 
 export default Home;
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -4,158 +4,87 @@ export const Container = styled.div`
   width: 100vw;
   height: 100vh;
 
+
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  justify-content: space-around;
   align-items: center;
   overflow: hidden;
+  padding: 1.5rem;
 
   color: var(--color-text-in-primary);
-  background: var(--color-primary);
+  background: #FDFDFD;
 `;
 
-export const ButtonContainer = styled.div`
+export const Header = styled.div`
+  background: #FDFDFD;
   display: flex;
+  width: 100%;
+  height: 100%;
+  flex: 1;
+  flex-direction: row;
   justify-content: center;
-  margin: 3rem;
+  color: #3E3B47;
 
-  a {
- 
-    padding: 10px;
-    width: 100%;
-    height: 8rem;
-    border-radius: 0.8rem;
-    font: bold 0.8rem Archivo;
-
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    text-decoration: none;
-    color: var(--color-button-text);
-
-    transition: background-color 0.2s;
-  }
-
-  a:first-child {
-    margin-right: 1.6rem;
-  }
-
-   a img {
-    width: 3.5rem;
-  }
-
-  .study {
-    background: var(--color-primary-lighter);
-  }
-
-  .give-classes {
-    background: var(--color-secundary);
-  }
-
-  .study:hover {
-    background: var(--color-primary-light);
-  }
-
-  .give-classes:hover {
-    background: var(--color-secundary-dark);
-  }
-
-  a img {
-    margin-right: 2.4rem;
-  }
-  
-  @media (min-width: 1100px) {
-    grid-area: buttons;
-      justify-content: flex-start;
-    }
-
-    a {
-      font-size: 2.4rem;
-    }
-  }
-`;
-
-export const TotalConnections = styled.span`
-  font-size: 1.4rem;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  img {
-    margin-left: 0.8rem;
-    height: 0.5 rem;
-  }
-
-  @media (min-width: 1100px) {
-    grid-area: total;
-    justify-self: end;
-  }
-`;
-
-export const TitleContainer = styled.div`
-  grid-area: logo;
-  text-align: center;
-  margin: 2rem;
-
-  h2 {
-    font-weight: 500;
-    font-size: 2.4rem;
-    line-height: 4.6rem;
-    margin-top: 0.8rem;
-  }
-
-  @media (min-width: 1100px) {
-    align-self: center;
-    text-align: left;
-    margin: 2rem;
-
-    h2 {
-      text-align: initial;
-      font-size: 3.6rem;
-    }
-  }
-`;
-
-export const LogoContainer = styled.div`
-  grid-area: hero;
-  text-align: center;
-  margin: 2rem;
-
-  img {
-    width: 100%;
-    padding: 2rem;
-  }
-
-  @media (min-width: 1100px) {
-    align-self: center;
-    text-align: left;
-    margin: 2rem;
-
-    img {
-      grid-area: hero;
-      height: 100%;
-    }
-  }
-`;
-
-export const Content = styled.div`
-  .hero-image {
-    padding: 2rem;
+  p {
     margin: 0 auto;
-    width: 20rem;
   }
 
-  @media (min-width: 1100px) {
-    max-width: 1100px;
-    display: grid;
-    grid-template-rows: 350px 1fr;
-    grid-template-columns: 2fr 1fr 1fr;
-    grid-template-areas: 'logo hero hero' 'buttons buttons total';
+  svg {
 
-    .hero-image {
-      grid-area: hero;
-      justify-self: end;
-    }
+    
   }
+
 `;
+export const Filter = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+  background: #F5F7FA;
+  border-radius: 1rem;
+  margin-top: 1rem;
+
+  button {
+    background: transparent;
+    width: 100%;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: 300;
+    font-size: 1.5rem;
+    padding: 0 1.5rem;
+    border-radius: 1rem;
+    line-height: 4rem;
+    border: none;
+
+    color: #8B96A8;
+  }
+
+  button:hover{
+    background: #6C63FF;
+    color: #F5F7FA;
+  }
+
+  button::select {
+    border: none;
+  }
+
+
+`;
+export const Title = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  width: 100%;
+  color: #3E3B47;
+  font-size: 4rem;
+`;
+export const Main = styled.main`
+  display: flex;
+  justify-content: center;
+`;
+export const Footer = styled.footer`
+background: #F5F7FA;
+  display: flex;
+  justify-content: center;
+`;
+
